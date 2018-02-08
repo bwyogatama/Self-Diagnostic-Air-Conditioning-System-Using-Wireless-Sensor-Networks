b3c23460cd0facc28c2497ea7ed4ffbe4f2f5d18
Eka Nur Prasetiani <36020571+enprasetiani@users.noreply.github.com>
2018-02-09 06:25:51 +0700

    Add files via upload

---
+++ Gateway & Electric Plug DAQ/Cek_1.ino
@@ -0,0 +1,20 @@
+const int analogIn = A0;              // inisialisasi pin A0 sebagai pembacaan sensor
+float sensor_Value = 0;               // setting nilai default pembacaan sensor 0
+float v_Out;                          // nilai tegangan output
+        
+void setup() {
+  Serial.begin (9600);                // inisialisasi komunikasi serial pada 960bps
+}
+
+void loop() {
+  sensor_Value = analogRead (A0);     // read the analog in value:
+  v_Out = 250 * (sensor_Value / 1024);// rumus perhitungan tegangan output
+  // // Mencetak hasil ke monitor serial
+  Serial.print ("vOut = ");
+  Serial.print(v_Out);
+  Serial.println("V");
+  // diberikan nilai tunda 1 s untuk pembacaan berikutnya
+  // Menampilkan pembacaan analog-to-digital converter setelah pembacaan terakhir
+  delay (1000);
+}
+
