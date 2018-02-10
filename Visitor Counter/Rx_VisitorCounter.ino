4588ff088dc4a1340af7dc3d0c575ae1fa3a2a1c
Eka Nur Prasetiani <36020571+enprasetiani@users.noreply.github.com>
2018-02-10 17:50:37 +0700

    Add files via upload

---
+++ Visitor Counter/Rx_VisitorCounter.ino
@@ -0,0 +1,82 @@
+#include <SoftwareSerial.h>
+#include <Printers.h>
+
+
+// Initialize XBee ZB client
+XBee xbee = XBee();
+XBeeResponse response = XBeeResponse();
+Rx16Response rx16 = Rx16Response();
+Rx64Response rx64 = Rx64Response();
+
+uint8_t payload[2]={};
+int statusLed = 13;
+
+uint8_t option = 0;
+uint8_t data = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); 
+  xbee.setSerial(Serial);
+  pinMode(statusLed, OUTPUT);
+//  xbee.begin(9600);  // start serial
+  flashLed(statusLed, 30, 50); 
+}
+
+void loop() {
+    
+    xbee.readPacket();
+    
+    if (xbee.getResponse().isAvailable()) {
+      // got something
+      
+      if (xbee.getResponse().getApiId() == RX_16_RESPONSE || xbee.getResponse().getApiId() == RX_64_RESPONSE) {
+        // got a rx packet
+        //flashLed(statusLed, 10, 5000);
+        //Serial.println("got a rx packet");
+        
+        if (xbee.getResponse().getApiId() == RX_16_RESPONSE) {
+                xbee.getResponse().getRx16Response(rx16);
+          option = rx16.getOption();
+          data = rx16.getData(0);
+        } else {
+                xbee.getResponse().getRx64Response(rx64);
+          option = rx64.getOption();
+          data = rx64.getData(0);
+        }
+        
+        //flashLed(statusLed, 10, 50);
+
+         
+         // Receiving and printing a float
+    payload[0]=char(rx16.getData(0));
+    payload[1]=int(rx16.getData(1));
+
+    Serial.print(char(rx16.getData(0)));
+    Serial.println(int(rx16.getData(1)));
+        
+      } else {
+        // not something we were expecting
+        flashLed(statusLed, 3, 1000);    
+      }
+    } else if (xbee.getResponse().isError()) {
+      flashLed(statusLed, 3, 1000);    
+      Serial.println("ERROR BUNG");
+    } 
+}
+
+void flashLed(int pin, int times, int wait) {
+    
+    for (int i = 0; i < times; i++) {
+      digitalWrite(pin, HIGH);
+      delay(wait);
+      digitalWrite(pin, LOW);
+      
+      if (i + 1 < times) {
+        delay(wait);
+      }
+    }
+}
+
+
+
---
+++ Visitor Counter/Tx_VisitorCounter.ino
@@ -0,0 +1,135 @@
+#include <Printers.h>
+#include <XBee.h>
+
+// Sensor ultrasonik 1
+#define trigPin1 6
+#define echoPin1 7
+// Sensor ultrasonik 2
+#define trigPin2 8
+#define echoPin2 9
+
+int hit1 = 0;
+int hit2 = 0;
+int count = 0;
+int maxDist = 15;
+
+// Initialize XBee ZB client
+XBee xbee = XBee();
+// Payload byte array
+uint8_t payload[2]={};
+// Send to coordinator
+// Create request for XBee ZB
+Tx16Request packet = Tx16Request(0x2222, payload, sizeof(payload));
+
+
+void setup() {
+  Serial.begin(9600); 
+  xbee.setSerial(Serial);
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+}
+
+void loop() {
+
+  xbee.send(packet);
+  
+  int duration1;
+  int distance1;
+  digitalWrite(trigPin1, LOW);
+  delayMicroseconds (2);
+  digitalWrite(trigPin1, HIGH);
+  delayMicroseconds (5);
+  digitalWrite(trigPin1, LOW);
+
+  duration1 = pulseIn (echoPin1, HIGH);
+  distance1 = (duration1/2)/29.1;
+
+  if (distance1 < maxDist){
+    //Serial.println ("Entering.......");
+    //Serial.println(distance1);
+    hit1 = 1;
+  }
+
+  // Check Sensor Ultrasonik 2
+  int duration2;
+  int distance2;
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds (2);
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds (5);
+  digitalWrite(trigPin2, LOW);
+
+  duration2 = pulseIn (echoPin2, HIGH);
+  distance2 = (duration2/2)/29.1;
+
+  if (distance2 < maxDist){
+    //Serial.println ("Exitting.......");
+    hit2 = 1;
+  } 
+
+  //entry was triggered, scan exit until exit is hit
+  while (hit1 == 1 && hit2 == 0){
+    // check sensor 2
+    int dur;
+    int dist;
+    digitalWrite(trigPin2, LOW);
+    delayMicroseconds (2);
+    digitalWrite(trigPin2, HIGH);
+    delayMicroseconds (5);
+    digitalWrite(trigPin2, LOW);
+
+    dur = pulseIn (echoPin2, HIGH);
+    dist = (dur/2)/29.1;
+
+    if (dist < maxDist ){
+      // reset the two values
+      hit1 = 0;
+      hit2 = 0;
+      count++;
+      payload[1]=count;  
+      Serial.println(" M A S U K");
+      Serial.print("Peopele = ");
+      Serial.println(count);
+      delay(200);
+      break;
+      
+    }  
+  }
+  //exit was triggered, scan entry until entry is hit
+  while (hit1 == 0 && hit2 == 1){
+    // check sensor 1
+    int dur;
+    int dist;
+    digitalWrite(trigPin1, LOW);
+    delayMicroseconds (2);
+    digitalWrite(trigPin1, HIGH);
+    delayMicroseconds (5);
+    digitalWrite(trigPin1, LOW);
+
+    dur = pulseIn (echoPin1, HIGH);
+    dist = (dur/2)/29.1;
+
+    if (dist < maxDist ){
+      // reset the two values
+      hit1 = 0;
+      hit2 = 0;
+      count--;
+      payload[1]=count;  
+      Serial.println(" K E L U A R ");
+      Serial.print(" Peopele = ");
+      Serial.println(count);
+      delay(200);
+            break;
+      
+    }
+    payload[0]='N'; 
+       
+      
+  }
+ 
+  xbee.send(packet);
+     
+  delay(1000);
+}
